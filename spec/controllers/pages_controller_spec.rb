b6c4d32e4d79c1c623848ce57094763726916d25
Jonathan He <jonathanhe@gmail.com>
2011-11-01 16:51:04 +0800

    Remove the default public/index.html, and some files name ending with ~ which were accidentally checked in during my initial commit

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,12 +12,12 @@ describe PagesController do
 
   describe "GET 'home'" do
     it "returns http success" do
-      get 'home'
+      get :home
       response.should be_success
     end
 
     it "should have the right title" do
-      get 'home'
+      get :home
       response.should have_selector('title',
                                     :content => "#{@base_title} | Home")
     end
@@ -25,12 +25,12 @@ describe PagesController do
 
   describe "GET 'contact'" do
     it "returns http success" do
-      get 'contact'
+      get :contact
       response.should be_success
     end
 
     it "should have the right title" do
-      get 'contact'
+      get :contact
       response.should have_selector('title',
                                     :content => "#{@base_title} | Contact us")
     end
@@ -38,15 +38,26 @@ describe PagesController do
 
   describe "GET 'about'" do
     it "returns http success" do
-      get 'about'
+      get :about
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get :about
+      response.should have_selector('title',
+                                    :content => "#{@base_title} | About us")
     end
   end
 
   describe "GET 'help'" do
     it "returns http success" do
-      get 'help'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get :help
+      response.should have_selector('title',
+                                    :content => "#{@base_title} | Help")
     end
   end
 
