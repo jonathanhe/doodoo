f008285e0b4010faefa58e2075f405eadcfd87e8
Jonathan He <jonathanhe@gmail.com>
2011-11-01 22:20:55 +0800

    Start to use header and footer partial, also fixed routes for some pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Doodoo::Application.routes.draw do
-  get "pages/home"
+#  get "pages/home"
+#  get "pages/contact"
+#  get "pages/about"
+#  get "pages/help"
 
-  get "pages/contact"
-
-  get "pages/about"
-
-  get "pages/help"
+  root              :to => 'pages#home'
+  match '/about',   :to => 'pages#about'
+  match '/contact', :to => 'pages#contact'
+  match '/help',    :to => 'pages#help'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
