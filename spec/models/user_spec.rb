5156b9f77ff4ea0b638812a617994cee7959774d
Jonathan He <jonathanhe@gmail.com>
2011-11-04 08:42:52 +0800

    Validates attributes in the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,6 +26,48 @@ describe User do
     no_email_user = User.new(@attr.merge(:email => ''))
     no_email_user.should_not be_valid
   end
+
+  it "should reject first_names that are too long" do
+    long_first_name = 'a' * 51
+    long_name_user =
+      User.new(@attr.merge(:first_name => long_first_name))
+    long_name_user.should_not be_valid
+  end
+
+  it "should reject last_names that are too long" do
+    long_last_name = 'a' * 51
+    long_name_user =
+      User.new(@attr.merge(:last_name => long_last_name))
+    long_name_user.should_not be_valid
+  end
+
+  it "should accept valid email addresses" do
+    addresses = %w[user@foo.com
+                   THE_USER@foo.bar.org
+                   first.last@foo.cn]
+    addresses.each do |email|
+      valid_email_user = User.new(@attr.merge(:email => email))
+      valid_email_user.should be_valid
+    end
+  end
+
+  it "should reject invalid email addresses" do
+    addresses = %w[user@foo,com
+                   user_at_foo.org
+                   user@example.
+                   @foo.org]
+    addresses.each do |email|
+      invalid_email_user = User.new(@attr.merge(:email => email))
+      invalid_email_user.should_not be_valid
+    end
+  end
+
+  it "should reject emails identical up to cases" do
+    upcased_email = @attr[:email].upcase
+    User.create!(@attr.merge(:email => upcased_email))
+    dup_email_user = User.new(@attr)
+    dup_email_user.should_not be_valid
+  end
 end
 # == Schema Information
 #
