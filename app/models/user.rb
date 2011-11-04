6a1e1b4540a94d3d7197474ba46bbbd1fc4bb9f1
Jonathan He <jonathanhe@gmail.com>
2011-11-04 08:16:03 +0800

    Validates attributes in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :email
+
+  validates :first_name, :presence => true
+  validates :last_name,  :presence => true
+  validates :email,      :presence => true
 end
 
 # == Schema Information
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,31 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @attr = { :first_name => 'Example',
+              :last_name  => 'User',
+              :email      => 'jon@example.com'}
+  end
+
+  it "should create a new instance given validate attributes" do
+    User.create!(@attr)
+  end
+
+  it "should require a first_name" do
+    no_first_name = User.new(@attr.merge(:first_name => ''))
+    no_first_name.should_not be_valid
+  end
+
+  it "should require a last_name" do
+    no_last_name = User.new(@attr.merge(:last_name => ''))
+    no_last_name.should_not be_valid
+  end
+
+  it "should require an email address" do
+    no_email_user = User.new(@attr.merge(:email => ''))
+    no_email_user.should_not be_valid
+  end
 end
 # == Schema Information
 #
