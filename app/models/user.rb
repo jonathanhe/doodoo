02368522abc4b9ab38c3a22c79d6dc999a2a9a57
Jonathan He <jonathanhe@gmail.com>
2011-11-04 16:13:29 +0800

    add encrypted_password attribute to the users table, but still storing plain password

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,17 +17,30 @@ class User < ActiveRecord::Base
   validates :password,   :presence     => true,
                          :confirmation => true,
                          :length       => { :within => 6..40 }
+
+  before_save :encrypt_password
+
+  private
+
+    def encrypt_password
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      string  # TODO: only temporary, fix this!!
+    end
 end
 
 # == Schema Information
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  first_name :string(255)
-#  last_name  :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  first_name         :string(255)
+#  last_name          :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,65 +11,67 @@ describe User do
             }
   end
 
-  it "should create a new instance given validate attributes" do
-    User.create!(@attr)
-  end
-
-  it "should require a first_name" do
-    no_first_name = User.new(@attr.merge(:first_name => ''))
-    no_first_name.should_not be_valid
-  end
-
-  it "should require a last_name" do
-    no_last_name = User.new(@attr.merge(:last_name => ''))
-    no_last_name.should_not be_valid
-  end
-
-  it "should require an email address" do
-    no_email_user = User.new(@attr.merge(:email => ''))
-    no_email_user.should_not be_valid
-  end
-
-  it "should reject first_names that are too long" do
-    long_first_name = 'a' * 51
-    long_name_user =
-      User.new(@attr.merge(:first_name => long_first_name))
-    long_name_user.should_not be_valid
-  end
-
-  it "should reject last_names that are too long" do
-    long_last_name = 'a' * 51
-    long_name_user =
-      User.new(@attr.merge(:last_name => long_last_name))
-    long_name_user.should_not be_valid
-  end
-
-  it "should accept valid email addresses" do
-    addresses = %w[user@foo.com
-                   THE_USER@foo.bar.org
-                   first.last@foo.cn]
-    addresses.each do |email|
-      valid_email_user = User.new(@attr.merge(:email => email))
-      valid_email_user.should be_valid
+  describe "test the presence of each attribute" do
+    it "should create a new instance given validate attributes" do
+      User.create!(@attr)
     end
-  end
 
-  it "should reject invalid email addresses" do
-    addresses = %w[user@foo,com
-                   user_at_foo.org
-                   user@example.
-                   @foo.org]
-    addresses.each do |email|
-      invalid_email_user = User.new(@attr.merge(:email => email))
-      invalid_email_user.should_not be_valid
+    it "should require a first_name" do
+      no_first_name = User.new(@attr.merge(:first_name => ''))
+      no_first_name.should_not be_valid
     end
-  end
 
-  it "should reject emails identical up to cases" do
-    upcased_email = @attr[:email].upcase
-    User.create!(@attr.merge(:email => upcased_email))
-    dup_email_user = User.new(@attr)
-    dup_email_user.should_not be_valid
+    it "should require a last_name" do
+      no_last_name = User.new(@attr.merge(:last_name => ''))
+      no_last_name.should_not be_valid
+    end
+
+    it "should require an email address" do
+      no_email_user = User.new(@attr.merge(:email => ''))
+      no_email_user.should_not be_valid
+    end
+
+    it "should reject first_names that are too long" do
+      long_first_name = 'a' * 51
+      long_name_user =
+        User.new(@attr.merge(:first_name => long_first_name))
+      long_name_user.should_not be_valid
+    end
+
+    it "should reject last_names that are too long" do
+      long_last_name = 'a' * 51
+      long_name_user =
+        User.new(@attr.merge(:last_name => long_last_name))
+      long_name_user.should_not be_valid
+    end
+
+    it "should accept valid email addresses" do
+      addresses = %w[user@foo.com
+                     THE_USER@foo.bar.org
+                     first.last@foo.cn]
+      addresses.each do |email|
+        valid_email_user = User.new(@attr.merge(:email => email))
+        valid_email_user.should be_valid
+      end
+    end
+
+    it "should reject invalid email addresses" do
+      addresses = %w[user@foo,com
+                     user_at_foo.org
+                     user@example.
+                     @foo.org]
+      addresses.each do |email|
+        invalid_email_user = User.new(@attr.merge(:email => email))
+        invalid_email_user.should_not be_valid
+      end
+    end
+
+    it "should reject emails identical up to cases" do
+      upcased_email = @attr[:email].upcase
+      User.create!(@attr.merge(:email => upcased_email))
+      dup_email_user = User.new(@attr)
+      dup_email_user.should_not be_valid
+    end
   end
 
   describe "password validations" do
@@ -99,16 +101,32 @@ describe User do
       User.new(hash).should_not be_valid
     end
   end
+
+  describe "password encryption" do
+
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+
+    it "should set the encrypted_password attribute" do
+      @user.encrypted_password.should_not be_blank
+    end
+  end
 end
 # == Schema Information
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  first_name :string(255)
-#  last_name  :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  first_name         :string(255)
+#  last_name          :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
