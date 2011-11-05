e2d6c069668d6186d7fe1c0477e5a73c3a974d64
Jonathan He <jonathanhe@gmail.com>
2011-11-05 08:57:12 +0800

    add happy test case for user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      # TODO: handle success case here!
+      redirect_to @user
     else
       @title = 'Sign up an account'
       render new_user_path
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -76,5 +76,28 @@ describe UsersController do
         #response.should render_template('new')
       end
     end
+
+    describe "success cases" do
+
+      before(:each) do
+        @attr = { :first_name => "Example",
+                  :last_name  => "User",
+                  :email      => "user@example.com",
+                  :password   => "foobar",
+                  :password_confirmation => "foobar"
+                }
+      end
+
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+    end
   end
 end
